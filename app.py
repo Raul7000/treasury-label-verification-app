937994614c36b5bfa201117fe0c7aa8e5840c6ee
Raul7000 <raulgonzales7000@gmail.com>
2026-06-14 13:32:32 -0700

    Fix Render port binding

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def home():
         <pre>{extracted_text}</pre>
         """
 
+
     return render_template('index.html')
 
 
@@ -67,4 +68,4 @@ def uploaded_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
